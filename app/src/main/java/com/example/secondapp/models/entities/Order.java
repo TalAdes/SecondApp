7cd675f43781a8d9640bdab7a17b1a407cc186e1
TalAdes <TalAdes@users.noreply.github.com>
2018-01-20 22:24:55 +0200

    Revert "s"
    This reverts commit 1fe1eb64d06a67bfad00450d61e866d5ab997c04.

--- app/src/main/java/com/example/secondapp/models/entities/Order.java
+++ app/src/main/java/com/example/secondapp/models/entities/Order.java
@@ -7,14 +7,74 @@ import java.util.Date;
  */
 
 public class Order {
-    private long    customerID ;
-    private String  orderStatus ;
-    private Date    startDate ;
-    private Date    finishDate ;
-    private int     startKilometer ;
-    private int     finishKilometer ;
-    private int     filedGas ;
-    private int     finallyCalculatedPrice ;
-    private long    orderID ;
-    private long    carID ;
+    private long numCustomer;
+    private String statusOrdr;
+    private long idCar;
+    private Date start;
+    private Date finish;
+    private int startKilometer;
+    private int finishKilometer;
+    private boolean oil;
+    private int liter;
+    private int total;
+    private long idOrder;
+
+    public Order(long NumCustomer, String StatusOrdr, long IdCar, Date Start, Date Finish, int StartKilometer,
+                 int FinishKilometer, boolean Oil, int Liter, int Total, long IdOrder) {
+        this.numCustomer = NumCustomer;
+        this.statusOrdr = StatusOrdr;
+        this.idCar = IdCar;
+        this.start = Start;
+        this.finish = Finish;
+        this.startKilometer = StartKilometer;
+        this.finishKilometer = FinishKilometer;
+        this.oil = Oil;
+        this.liter = Liter;
+        this.total = Total;
+        this.idOrder = IdOrder;
+    }
+
+    public long getNumCustomer() {return numCustomer;}
+
+    public void setNumCustomer(long numCustomer) {this.numCustomer = numCustomer;}
+
+    public String getStatusOrdr() {return statusOrdr;}
+
+    public void setStatusOrdr(String statusOrdr) {this.statusOrdr = statusOrdr;}
+
+    public long getIdCar() {return idCar;}
+
+    public void setIdCar(long idCar) {this.idCar = idCar;}
+
+    public Date getStart() {return start;}
+
+    public void setStart(Date start) {this.start = start;}
+
+    public Date getFinish() {return finish;}
+
+    public void setFinish(Date finish) {this.finish = finish;}
+
+    public int getStartKilometer() {return startKilometer;}
+
+    public void setStartKilometer(int startKilometer) {this.startKilometer = startKilometer;}
+
+    public int getFinishKilometer() {return finishKilometer;}
+
+    public void setFinishKilometer(int finishKilometer) {this.finishKilometer = finishKilometer;}
+
+    public boolean isOil() {return oil;}
+
+    public void setOil(boolean oil) {this.oil = oil;}
+
+    public int getLiter() {return liter;}
+
+    public void setLiter(int liter) {this.liter = liter;}
+
+    public int getTotal() {return total;}
+
+    public void setTotal(int total) {this.total = total;}
+
+    public long getIdOrder() {return idOrder;}
+
+    public void setIdOrder(long idOrder) {this.idOrder = idOrder;}
 }
